144e58d04535761ef6e6d9f303f4b7ca55c391d8
Jake Crawford <jjc2718@gmail.com>
2020-12-03 11:56:11 -0500

    add config file and allow code to be used as a package

---
+++ mpmp/config.py
@@ -0,0 +1,13 @@
+import pathlib
+
+repo_root = pathlib.Path(__file__).resolve().parent.parent
+
+# important subdirectories
+data_dir = repo_root / 'data'
+results_dir = repo_root / 'results'
+
+# location of saved expression data
+pancan_data = data_dir / 'pancancer_data.pkl'
+rnaseq_data = data_dir / 'tcga_expression_matrix_processed.tsv.gz'
+sample_counts = data_dir / 'tcga_sample_counts.tsv'
+sample_info = data_dir / 'tcga_sample_identifiers.tsv'
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='mpmp',
+    author='Jake Crawford',
+    version='0.0.1',
+    description='Multimodal Pan-cancer Mutation Prediction',
+    packages=['mpmp']
+)
