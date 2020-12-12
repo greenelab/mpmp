34c5c179b1b0cee29e1f6360bc644090962322c0
Jake Crawford <jjc2718@gmail.com>
2020-12-12 14:50:29 -0500

    add train model code

--- mpmp/config.py
+++ mpmp/config.py
@@ -31,6 +31,12 @@ filter_prop = 0.05
 # filter cancer types with less than this number of mutated samples
 filter_count = 15
 
+# hyperparameters for classification experiments
+folds = 3
+max_iter = 200
+alphas = [0.1, 0.13, 0.15, 0.2, 0.25, 0.3]
+l1_ratios = [0.15, 0.16, 0.2, 0.25, 0.3, 0.4]
+
 # repo/commit information to retrieve precomputed cancer gene information
 # this is used in data_utilities.py
 top50_base_url = "https://github.com/greenelab/BioBombe/raw"
--- mpmp/utilities/classify_utilities.py
+++ mpmp/utilities/classify_utilities.py
@@ -5,17 +5,19 @@ Many of these functions are adapted from:
 https://github.com/greenelab/BioBombe/blob/master/9.tcga-classify/scripts/tcga_util.py
 """
 import pandas as pd
-# from sklearn.pipeline import Pipeline
-# from sklearn.linear_model import SGDClassifier
-# from sklearn.metrics import (
-#     roc_auc_score,
-#     roc_curve,
-#     precision_recall_curve,
-#     average_precision_score
-# )
-from sklearn.model_selection import StratifiedKFold
-# from sklearn.model_selection import cross_val_predict
-# from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import (
+    roc_auc_score,
+    roc_curve,
+    precision_recall_curve,
+    average_precision_score
+)
+from sklearn.model_selection import (
+    StratifiedKFold,
+    cross_val_predict,
+    GridSearchCV,
+)
 
 import mpmp.config as cfg
 import mpmp.utilities.tcga_utilities as tu
@@ -66,28 +68,21 @@ def run_cv_cancer_type(data_model,
                                                    standardize_columns,
                                                    data_model.subset_mad_genes)
 
-    # try:
-    #     # also ignore warnings here, same deal as above
-    #     with warnings.catch_warnings():
-    #         warnings.simplefilter("ignore")
-    #         model_results = train_model(
-    #             X_train=X_train_df,
-    #             X_test=X_test_df,
-    #             y_train=y_train_df,
-    #             alphas=cfg.alphas,
-    #             l1_ratios=cfg.l1_ratios,
-    #             seed=data_model.seed,
-    #             n_folds=cfg.folds,
-    #             max_iter=cfg.max_iter
-    #         )
-    #         (cv_pipeline,
-    #          y_pred_train_df,
-    #          y_pred_test_df,
-    #          y_cv_df) = model_results
-    # except ValueError:
-    #     raise OneClassError(
-    #         'Only one class present in test set for gene: {}\n'.format(gene)
-    #     )
+        model_results = train_model(
+            X_train=X_train_df,
+            X_test=X_test_df,
+            y_train=y_train_df,
+            alphas=cfg.alphas,
+            l1_ratios=cfg.l1_ratios,
+            seed=data_model.seed,
+            n_folds=cfg.folds,
+            max_iter=cfg.max_iter
+        )
+        (cv_pipeline,
+         y_pred_train_df,
+         y_pred_test_df,
+         y_cv_df) = model_results
+        print(y_pred_train_df[:5])
 
     # # TODO: separate below into another function (one returns raw results)
 
@@ -121,6 +116,84 @@ def run_cv_cancer_type(data_model,
     # results['gene_coef'].append(coef_df)
 
     return results
+
+
+def train_model(X_train,
+                X_test,
+                y_train,
+                alphas,
+                l1_ratios,
+                seed,
+                n_folds=4,
+                max_iter=1000):
+    """
+    Build the logic and sklearn pipelines to train x matrix based on input y
+
+    Arguments
+    ---------
+    X_train: pandas DataFrame of feature matrix for training data
+    X_test: pandas DataFrame of feature matrix for testing data
+    y_train: pandas DataFrame of processed y matrix (output from align_matrices())
+    alphas: list of alphas to perform cross validation over
+    l1_ratios: list of l1 mixing parameters to perform cross validation over
+    n_folds: int of how many folds of cross validation to perform
+    max_iter: the maximum number of iterations to test until convergence
+
+    Returns
+    ------
+    The full pipeline sklearn object and y matrix predictions for training, testing,
+    and cross validation
+    """
+    # Setup the classifier parameters
+    clf_parameters = {
+        "classify__loss": ["log"],
+        "classify__penalty": ["elasticnet"],
+        "classify__alpha": alphas,
+        "classify__l1_ratio": l1_ratios,
+    }
+
+    estimator = Pipeline(
+        steps=[
+            (
+                "classify",
+                SGDClassifier(
+                    random_state=seed,
+                    class_weight="balanced",
+                    loss="log",
+                    max_iter=max_iter,
+                    tol=1e-3,
+                ),
+            )
+        ]
+    )
+
+    cv_pipeline = GridSearchCV(
+        estimator=estimator,
+        param_grid=clf_parameters,
+        n_jobs=-1,
+        cv=n_folds,
+        scoring="roc_auc",
+        return_train_score=True,
+        iid=False
+    )
+
+    # Fit the model
+    cv_pipeline.fit(X=X_train, y=y_train.status)
+
+    # Obtain cross validation results
+    y_cv = cross_val_predict(
+        cv_pipeline.best_estimator_,
+        X=X_train,
+        y=y_train.status,
+        cv=n_folds,
+        method="decision_function",
+    )
+
+    # Get all performance results
+    y_predict_train = cv_pipeline.decision_function(X_train)
+    y_predict_test = cv_pipeline.decision_function(X_test)
+
+    return cv_pipeline, y_predict_train, y_predict_test, y_cv
 
 
 def split_stratified(data_df,
