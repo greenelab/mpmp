c7a134f5f3ab79d0e15990c572d0a2555d04893c
Jake Crawford <jjc2718@gmail.com>
2021-02-18 09:49:25 -0500

    run using compressed data for methylation

--- 02_classify_compressed/run_mutation_compressed.py
+++ 02_classify_compressed/run_mutation_compressed.py
@@ -101,7 +101,8 @@ if __name__ == '__main__':
 
     # process command line arguments
     io_args, model_options = process_args()
-    sample_info_df = du.load_sample_info(verbose=io_args.verbose)
+    sample_info_df = du.load_sample_info(model_options.training_data,
+                                         verbose=io_args.verbose)
 
     # create results dir and subdir for experiment if they don't exist
     experiment_dir = Path(io_args.results_dir, 'gene').resolve()
