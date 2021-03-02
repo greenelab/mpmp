ccd8cb6647ddddc519de0c25773f2700aaa6b4c2
Jake Crawford <jjc2718@gmail.com>
2021-03-02 11:19:07 -0500

    add converted notebooks

--- 00_download_data/nbconverted/3_explore_data.py
+++ 00_download_data/nbconverted/3_explore_data.py
@@ -22,10 +22,12 @@ import mpmp.utilities.data_utilities as du
 # In[2]:
 
 
+DATA_TYPE = 'mut_sigs'
+
 # load gene/classification info and sample/cancer type info
 print('Loading gene label data...', file=sys.stderr)
 genes_df = du.load_vogelstein()
-sample_info_df = du.load_sample_info(verbose=True)
+sample_info_df = du.load_sample_info(DATA_TYPE, verbose=True)
 
 # load mutation info
 # this returns a tuple of dataframes, unpack it below
@@ -40,38 +42,42 @@ pancancer_data = du.load_pancancer_data(verbose=True)
 # In[3]:
 
 
-# load expression data
-rnaseq_df = du.load_expression_data(verbose=True)
+# load relevant data
+data_df = du.load_raw_data(DATA_TYPE, verbose=True)
 
 # standardize columns of expression dataframe
-print('Standardizing columns of expression data...', file=sys.stderr)
-rnaseq_df[rnaseq_df.columns] = StandardScaler().fit_transform(rnaseq_df[rnaseq_df.columns])
+if DATA_TYPE in cfg.standardize_data_types:
+    print('Standardizing columns of {} data...'.format(DATA_TYPE), 
+          file=sys.stderr)
+    data_df[data_df.columns] = StandardScaler().fit_transform(data_df[data_df.columns])
 
-print(rnaseq_df.shape)
-rnaseq_df.iloc[:5, :5]
-
-
-# In[4]:
-
-
-# load methylation data
-methylation_df = du.load_methylation_data(verbose=True)
-
-# standardize columns of methylation dataframe
-# decided not to do this for now, unnormalized beta values seem to be informative
-# print('Standardizing columns of methylation data...', file=sys.stderr)
-# methylation_df[methylation_df.columns] = StandardScaler().fit_transform(
-#     methylation_df[methylation_df.columns])
-
-print(methylation_df.shape)
-methylation_df.iloc[:5, :5]
+print(data_df.shape)
+data_df.iloc[:5, :5]
 
 
-# First, let's compare low-dimensional representations of gene expression and DNA methylation data. These representations should be somewhat similar between data types (DNA methylation affects/correlates with gene expression), and we should see a decent separation between cancer types in the plots.
+# First, let's look at the low-dimensional representation of the chosen data type.
 # 
 # We'll choose a few cancer types that are similar to one another (LUSC/LUAD, LGG/GBM) and a few that should be dissimilar (BRCA, THCA).
 
-# In[5]:
+# In[25]:
+
+
+assert sample_info_df.index.equals(data_df.index)
+
+# data_cancer_types = sorted(sample_info_df.cancer_type.unique())
+data_cancer_types = ['LUAD', 'LUSC', 'THCA', 'LGG', 'GBM', 'BRCA']
+# data_cancer_types = ['LUAD', 'LUSC', 'LAML']
+data_types_df = (data_df
+    .merge(sample_info_df, left_index=True, right_index=True)
+    .query('cancer_type in @data_cancer_types')
+    .drop(columns=['sample_type', 'id_for_stratification'])
+    .reset_index()
+)
+print(data_types_df.cancer_type.unique())
+data_types_df.iloc[:5, -5:]
+
+
+# In[26]:
 
 
 from sklearn.decomposition import PCA
@@ -80,77 +86,44 @@ from umap import UMAP
 sns.set({'figure.figsize': (20, 8)})
 fig, axarr = plt.subplots(1, 2)
 
-assert sample_info_df.index.equals(rnaseq_df.index)
-
 pca = PCA(n_components=2)
-X_proj_pca = pca.fit_transform(rnaseq_df)
+X_proj_pca = pca.fit_transform(data_types_df.drop(columns=['sample_id', 'cancer_type']))
 reducer = UMAP(n_components=2, random_state=42)
-X_proj_umap = reducer.fit_transform(rnaseq_df)
+X_proj_umap = reducer.fit_transform(data_types_df.drop(columns=['sample_id', 'cancer_type']))
 
-enum_samples_df = sample_info_df.reset_index()
-rnaseq_cancer_types = sorted(sample_info_df.cancer_type.unique())
-# rnaseq_cancer_types = ['LUAD', 'LUSC', 'THCA', 'LGG', 'GBM', 'BRCA']
-for i, cancer_type in enumerate(rnaseq_cancer_types):
-    ixs = enum_samples_df.index[enum_samples_df['cancer_type'] == cancer_type].tolist()
-    axarr[0].scatter(X_proj_pca[ixs, 0], X_proj_pca[ixs, 1], label=cancer_type, s=3)
-    axarr[1].scatter(X_proj_umap[ixs, 0], X_proj_umap[ixs, 1], label=cancer_type, s=3)
+for i, cancer_type in enumerate(data_cancer_types):
+    ixs = data_types_df.index[data_types_df.cancer_type == cancer_type].tolist()
+    axarr[0].scatter(X_proj_pca[ixs, 0], X_proj_pca[ixs, 1], label=cancer_type, s=5)
+    axarr[1].scatter(X_proj_umap[ixs, 0], X_proj_umap[ixs, 1], label=cancer_type, s=5)
     
 axarr[0].set_xlabel('PC1')
 axarr[0].set_ylabel('PC2')
-axarr[0].set_title('PCA projection of TCGA expression data, colored by cancer type')
+axarr[0].set_title('PCA projection of {} data, colored by cancer type'.format(DATA_TYPE))
 axarr[0].legend()
 axarr[1].set_xlabel('UMAP dimension 1')
 axarr[1].set_ylabel('UMAP dimension 2')
-axarr[1].set_title('UMAP projection of TCGA expression data, colored by cancer type')
+axarr[1].set_title('UMAP projection of {} data, colored by cancer type'.format(DATA_TYPE))
 axarr[1].legend()
 
 
-# In[6]:
-
-
-sample_overlap = rnaseq_df.index.intersection(methylation_df.index)
-print(len(rnaseq_df), len(methylation_df), len(sample_overlap))
-
-
-# In[7]:
-
-
-sns.set({'figure.figsize': (20, 8)})
-fig, axarr = plt.subplots(1, 2)
-
-methylation_filtered_df = methylation_df.reindex(sample_overlap)
-enum_samples_df = (
-    sample_info_df.reindex(sample_overlap)
-                  .reset_index()
-)
-
-pca = PCA(n_components=2)
-X_proj_pca = pca.fit_transform(methylation_filtered_df)
-reducer = UMAP(n_components=2, random_state=42)
-X_proj_umap = reducer.fit_transform(methylation_filtered_df)
-
-me_cancer_types = sorted(sample_info_df.cancer_type.unique())
-# me_cancer_types = ['LUAD', 'LUSC', 'THCA', 'LGG', 'GBM', 'BRCA']
-for i, cancer_type in enumerate(me_cancer_types):
-    ixs = enum_samples_df.index[enum_samples_df['cancer_type'] == cancer_type].tolist()
-    axarr[0].scatter(X_proj_pca[ixs, 0], X_proj_pca[ixs, 1], label=cancer_type, s=3)
-    axarr[1].scatter(X_proj_umap[ixs, 0], X_proj_umap[ixs, 1], label=cancer_type, s=3)
-    
-axarr[0].set_xlabel('PC1')
-axarr[0].set_ylabel('PC2')
-axarr[0].set_title('PCA projection of TCGA methylation data, colored by cancer type')
-axarr[0].legend()
-axarr[1].set_xlabel('UMAP dimension 1')
-axarr[1].set_ylabel('UMAP dimension 2')
-axarr[1].set_title('UMAP projection of TCGA methylation data, colored by cancer type')
-axarr[1].legend()
-
-
-# Now we want to dig a bit deeper into LGG and GBM. It's fairly well-known that IDH1 mutation status defines distinct subtypes in both classes of brain tumors. We'll compare methylation and gene expression in IDH1-mutated vs. non-mutated samples, expecting to see a separation in our low dimensional representation.
+# Now we want to dig a bit deeper into LGG and GBM, using expression and methylation data. It's fairly well-known that IDH1 mutation status defines distinct subtypes in both classes of brain tumors. We'll compare methylation and gene expression in IDH1-mutated vs. non-mutated samples, expecting to see a separation in our low dimensional representation.
 # 
 # IDH1 plays a direct role in DNA methylation, so we anticipate that this separation between mutated and non-mutated samples will be slightly clearer in the methylation data.
 
-# In[8]:
+# In[5]:
+
+
+# load relevant data
+rnaseq_df = du.load_raw_data('expression', verbose=True)
+print('Standardizing columns of expression data...', file=sys.stderr)
+rnaseq_df[rnaseq_df.columns] = StandardScaler().fit_transform(rnaseq_df[rnaseq_df.columns])
+
+methylation_df = du.load_raw_data('me_27k', verbose=True)
+print(methylation_df.shape)
+methylation_df.iloc[:5, :5]
+
+
+# In[6]:
 
 
 from mpmp.utilities.tcga_utilities import process_y_matrix
@@ -188,7 +161,7 @@ def generate_labels(gene, classification):
     return y_df
 
 
-# In[9]:
+# In[7]:
 
 
 gene = 'IDH1'
@@ -201,7 +174,7 @@ print(y_df.shape)
 y_df.tail()
 
 
-# In[10]:
+# In[8]:
 
 
 # generate UMAP 2-dimensional representations of data
@@ -220,10 +193,10 @@ def shuffle_cols(input_df):
     return shuf_df
 
 # get samples that are present in all 3 datasets (expression, methylation, mutations)
-ix_overlap = y_df.index.intersection(rnaseq_df.index).intersection(methylation_filtered_df.index)
+ix_overlap = y_df.index.intersection(rnaseq_df.index).intersection(methylation_df.index)
 y_mut_df = y_df.loc[ix_overlap, :]
 rnaseq_mut_df = rnaseq_df.loc[ix_overlap, :]
-me_mut_df = methylation_filtered_df.loc[ix_overlap, :]
+me_mut_df = methylation_df.loc[ix_overlap, :]
 
 if shuffle:
     rnaseq_mut_df = shuffle_cols(rnaseq_mut_df)
@@ -236,7 +209,7 @@ print(X_proj_rnaseq.shape)
 print(X_proj_me.shape)
 
 
-# In[11]:
+# In[9]:
 
 
 gene_label = '{} mutant'.format(gene)
@@ -255,7 +228,7 @@ rnaseq_proj_df = pd.DataFrame({
 me_proj_df.head()
 
 
-# In[12]:
+# In[10]:
 
 
 sns.set({'figure.figsize': (20, 8)})
