abdab1774da62b007597149024a7e7fa886fc96a
Jake Crawford <jjc2718@gmail.com>
2021-03-05 16:56:19 -0500

    subsample features to speed up tests
    also track data needed for tests, so we can set up GH actions

--- mpmp/test_config.py
+++ mpmp/test_config.py
@@ -12,9 +12,9 @@ test_pancan_data = test_data_dir / 'pancancer_data_subsampled.pkl'
 test_stratified_results = str(test_data_dir / 'stratified_results_{}.tsv')
 
 # gene mutation info used in tests
-test_genes = ['TP53', 'KRAS', 'ARID1A']
+test_genes = ['TP53', 'EGFR', 'ARID1A']
 
 # gene/training data/classification combos for stratified CV model tests
 stratified_gene_info = [('TP53', 'TSG'),
-                        ('KRAS', 'Oncogene'),
+                        ('EGFR', 'Oncogene'),
                         ('ARID1A', 'TSG')]
