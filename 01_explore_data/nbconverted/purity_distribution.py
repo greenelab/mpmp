bcd0f0e8a1c4bf85d1455c156201a7d819c02f4a
Jake Crawford <jjc2718@gmail.com>
2021-03-12 11:32:18 -0500

    add some detail about purity distribution script

--- 01_explore_data/nbconverted/purity_distribution.py
+++ 01_explore_data/nbconverted/purity_distribution.py
@@ -3,7 +3,7 @@
 
 # ## Explore tumor purity data
 
-# In[5]:
+# In[1]:
 
 
 import os
@@ -17,14 +17,14 @@ import mpmp.config as cfg
 import mpmp.utilities.data_utilities as du
 
 
-# In[6]:
+# In[2]:
 
 
 purity_df = pd.read_csv(cfg.tumor_purity_data, sep='\t', index_col=0)
 purity_df.head()
 
 
-# In[7]:
+# In[3]:
 
 
 # visualize distribution of tumor purity values
@@ -34,7 +34,11 @@ plt.gca().axvline(x=purity_df.purity.median(), linestyle='--', color='red')
 plt.xlabel('Tumor purity')
 
 
-# In[8]:
+# The median tumor purity value seems to be a bit higher than 0.5 (this is probably due to filtering in some cancer types for high purity samples).
+# 
+# This should be reasonable enough to start with for classification, although I don't think it's the best way to do predictive modeling of this data. Really, we should just do regression, but we could also explore other binarization methods, or binarizing by cancer type.
+
+# In[4]:
 
 
 # visualize distribution of tumor purity values, by cancer type
@@ -42,7 +46,7 @@ sample_info_df = du.load_sample_info('expression')
 sample_info_df.head()
 
 
-# In[11]:
+# In[5]:
 
 
 purity_cancer_df = (purity_df[['purity']]
@@ -52,7 +56,7 @@ purity_cancer_df = (purity_df[['purity']]
 purity_cancer_df.head()
 
 
-# In[45]:
+# In[6]:
 
 
 sns.set({'figure.figsize': (8, 5)})
@@ -63,3 +67,7 @@ g = sns.kdeplot(data=purity_cancer_df, x='purity', hue='cancer_type', legend=Fal
 plt.gca().axvline(x=purity_df.purity.median(), linestyle='--', color='red')
 plt.xlabel('Tumor purity')
 
+
+# When we facet by cancer type, we can see that quite a few cancers have a peak closer to 0.8/0.9, probably due to filtering for high-purity samples. Others have many more low-purity samples (e.g. [pancreatic cancer](https://www.cancer.gov/about-nci/organization/ccg/research/structural-genomics/tcga/studied-cancers/pancreatic)), and some are roughly centered around 0.5.
+# 
+# In the future, we could look further into these differences between cancer types, depending on the results of the prediction experiments.
