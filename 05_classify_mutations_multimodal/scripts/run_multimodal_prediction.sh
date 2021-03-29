8e139dba8d3f0d22358c5e2d0f160c246cba9202
Jake Crawford <jjc2718@gmail.com>
2021-03-29 17:28:37 -0400

    look at results without any MAD filtering

--- 05_classify_mutations_multimodal/scripts/run_multimodal_prediction.sh
+++ 05_classify_mutations_multimodal/scripts/run_multimodal_prediction.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-RESULTS_DIR=./05_classify_mutations_multimodal/results/pilot_results
+RESULTS_DIR=./05_classify_mutations_multimodal/results/pilot_results_all_feats
 ERRORS_DIR=./multimodal_pilot_errors
 GENES="TP53 KRAS EGFR PIK3CA IDH1 SETD2 TET2"
 
@@ -14,7 +14,7 @@ for seed in 42 1; do
     cmd+="--custom_genes ${GENES} "
     cmd+="--results_dir $RESULTS_DIR "
     cmd+="--training_data expression me_27k "
-    cmd+="--subset_mad_genes 5000 "
+    cmd+="--subset_mad_genes -1 "
     cmd+="--seed $seed "
     cmd+="2>$ERRORS_DIR/errors_expression_me_27k.txt"
     echo "Running: $cmd"
@@ -25,7 +25,7 @@ for seed in 42 1; do
     cmd+="--custom_genes ${GENES} "
     cmd+="--results_dir $RESULTS_DIR "
     cmd+="--training_data expression me_450k "
-    cmd+="--subset_mad_genes 5000 "
+    cmd+="--subset_mad_genes -1 "
     cmd+="--n_dim None 5000 "
     cmd+="--seed $seed "
     cmd+="2>$ERRORS_DIR/errors_expression_me_450k.txt"
@@ -37,7 +37,7 @@ for seed in 42 1; do
     cmd+="--custom_genes ${GENES} "
     cmd+="--results_dir $RESULTS_DIR "
     cmd+="--training_data me_27k me_450k "
-    cmd+="--subset_mad_genes 5000 "
+    cmd+="--subset_mad_genes -1 "
     cmd+="--n_dim None 5000 "
     cmd+="--seed $seed "
     cmd+="2>$ERRORS_DIR/errors_me_27k_me_450k.txt"
@@ -49,7 +49,7 @@ for seed in 42 1; do
     cmd+="--custom_genes ${GENES} "
     cmd+="--results_dir $RESULTS_DIR "
     cmd+="--training_data expression me_27k me_450k "
-    cmd+="--subset_mad_genes 5000 "
+    cmd+="--subset_mad_genes -1 "
     cmd+="--n_dim None None 5000 "
     cmd+="--seed $seed "
     cmd+="2>$ERRORS_DIR/errors_all.txt"
