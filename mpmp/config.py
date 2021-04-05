935a18154b274400cc960642156248e0a127f599
Jake Crawford <jjc2718@gmail.com>
2021-04-05 16:00:33 -0400

    add figure generation script + generate expression figure

--- mpmp/config.py
+++ mpmp/config.py
@@ -10,6 +10,13 @@ results_dirs = {
     'purity': repo_root / '04_predict_tumor_purity' / 'results',
     'multimodal': repo_root / '05_classify_mutations_multimodal' / 'results',
 }
+images_dirs = {
+    'mutation': repo_root / '02_classify_mutations' / 'images',
+    'cancer_type': repo_root / '03_classify_cancer_type' / 'images',
+    'purity': repo_root / '04_predict_tumor_purity' / 'images',
+    'multimodal': repo_root / '05_classify_mutations_multimodal' / 'images',
+}
+paper_figures_dir = repo_root / 'figures'
 
 # locations of saved data files
 raw_data_dir = data_dir / 'raw'
