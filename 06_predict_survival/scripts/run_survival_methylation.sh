63b2f022e4e52791a1122d2cc3dea2827c2a37c2
Jake Crawford <jjc2718@gmail.com>
2021-11-02 16:58:41 -0400

    clean up survival bash scripts

--- 06_predict_survival/scripts/run_survival_methylation.sh
+++ 06_predict_survival/scripts/run_survival_methylation.sh
@@ -4,36 +4,9 @@ mkdir -p $ERRORS_DIR
 
 for seed in 42 1; do
 
-    # first run non-omics covariate baseline
-    # results_dir=./06_predict_survival/results/me_baseline/results_baseline/
-    # cmd="python 06_predict_survival/run_survival_prediction.py "
-    # cmd+="--results_dir $results_dir "
-    # cmd+="--seed $seed "
-    # cmd+="--overlap_data_types expression me_27k me_450k "
-    # cmd+="--training_data baseline "
-    # cmd+="2>$ERRORS_DIR/errors_baseline_s${seed}.txt"
-    # echo "Running: $cmd"
-    # eval $cmd
+    for n_dim in 10 100 500 1000 5000; do
 
-    # for n_dim in 10 100 500 1000 5000; do
-    for n_dim in 10 100; do
-
-    #     # raw data
-    #     results_dir=./06_predict_survival/results/me_ridge/results_${n_dim}_raw/
-    #     for data_type in expression me_27k me_450k; do
-    #         cmd="python 06_predict_survival/run_survival_prediction.py "
-    #         cmd+="--n_dim $n_dim "
-    #         cmd+="--results_dir $results_dir "
-    #         cmd+="--seed $seed "
-    #         cmd+="--subset_mad_genes $n_dim "
-    #         cmd+="--overlap_data_types expression me_27k me_450k "
-    #         cmd+="--training_data ${data_type} "
-    #         cmd+="2>$ERRORS_DIR/errors_${data_type}_s${seed}.txt"
-    #         echo "Running: $cmd"
-    #         eval $cmd
-    #     done
-
-    #     # top n_dim PCA features
+        # top n_dim PCA features
         results_dir=./06_predict_survival/results/me_ridge/results_${n_dim}_pca/
         for data_type in expression me_27k me_450k; do
             cmd="python 06_predict_survival/run_survival_prediction.py "
@@ -48,4 +21,5 @@ for seed in 42 1; do
         done
 
     done
+
 done
